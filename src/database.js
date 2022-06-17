a55d15ee3755f42e356bc630af7bf2a55f2b7bac
FRANCO RODRIGUEZ PACIELLO <francorodriguez429@gmail.com>
2022-06-17 14:23:49 -0300

    Test

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const URI = 'mongodb://localhost/mern-tasks'
 
 mongoose.connect(URI)
-    .then(db => console.log('DB is connected'))
+    .then(db => console.log('DB is connectedddddd'))
     .catch(err => console.error(err));
 
 module.exports = mongoose;
