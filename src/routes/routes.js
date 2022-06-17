f32b9e4695c8e18a172e7351692adb40c847a8a0
FRANCO RODRIGUEZ PACIELLO <francorodriguez429@gmail.com>
2022-06-17 14:43:13 -0300

    FINISH CRUD

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,11 +10,29 @@ router.get('/', async (req,res) => {
    
 });
 
+router.get('/:id', async (req,res) => {
+    await Task.findById(req.params.id);
+    res.json(task);
+});
+
 router.post('/',async (req, res) => {
     const {title, description} = req.body;
     const task = new Task({  title, description  })
     await task.save();
     res.json({status: 'Tarea guardada'})
+})
+
+router.put('/:id', async (req,res) => {
+    const {title,description} = req.body;
+    const newTask = {title,description};
+    await Task.findByIdAndUpdate(req.params.id,newTask);
+   
+    res.json({status: 'tarea actualizada'});
+});
+
+router.delete('/:id', async (req, res) =>  {
+    await Task.findByIdAndRemove( req.params.id);
+    res.json({status: 'deleted task'});
 })
 
 module.exports = router;
